701eebea66de015289c313ddfc75ff2dc8dfd77d
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-06-26 22:16:34 -0300

    Finished cart page

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CarrinhoService } from '../carrinho.service';
+import { IprodutoCarrinho } from '../produtos/produtos';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrinho',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrinhoComponent implements OnInit {
 
-  constructor() { }
+  itensCarrinho: IprodutoCarrinho[] = [];
+  total = 0;
+
+  constructor(
+    public carrinhoService : CarrinhoService,
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
+    this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calculaTotal()
+  }
+
+  calculaTotal(){
+    this.total = this.itensCarrinho.reduce((prev, curr) => prev + (curr.preco * curr.quantidade),0);
+  }
+
+  removerProdutoCarrinho(produtoId : number){
+    this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId)
+    this.carrinhoService.removerProdutoCarrinho(produtoId)
+    this.calculaTotal()
+  }
+
+  comprar(){
+    alert("Parabéns, você finalizou a sua compra !")
+    this.carrinhoService.limparCarrinho()
+    this.router.navigate(["produtos"]);
   }
 
 }
